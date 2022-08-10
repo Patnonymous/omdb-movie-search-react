7a8d84da2e45ae8f2a0a370947f7fa8711a004e6
Patrick_Williamson <patrickw9080@outlook.com>
2022-08-10 15:40:31 -0400

    Added search functionality.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // Imports - React
-import React from 'react';
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Imports - Components
 import MovieCard from './MovieCard';
@@ -15,31 +14,41 @@ const API_URL = 'http://www.omdbapi.com/?apikey=2e690dce'
 
 const App = () => {
 
+    // Movies state is default to an empty array.
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        // Set the movies state.
+        setMovies(data.Search);
     };
 
-    useEffect(() => {
-        searchMovies('batman');
-    }, []);
+
 
 
     return (
-        <div className='app'>
+        <div className="app">
             <h1>MovieLand</h1>
 
-
-            <div className='search'>
-                <input placeholder='Movie title...' value='Superman' onChange={() => { }} />
-                <img src={SearchIcon} alt='Search' onClick={() => { }} />
+            <div className="search">
+                <input placeholder="Movie title..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                <img src={SearchIcon} alt="Search" onClick={() => searchMovies(searchTerm)} />
             </div>
 
-            <div className='container'>
-
-            </div>
+            {movies?.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No Movies Found</h2>
+                </div>
+            )}
         </div>
     );
 };
